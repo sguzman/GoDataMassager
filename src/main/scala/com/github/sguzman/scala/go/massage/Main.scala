0ff36f7edfba693613044fc8d4cad2e713811018
Salvador Guzman <guzmansalv@gmail.com>
2018-03-27 19:21:43 -0700

    done

--- src/main/scala/com/github/sguzman/scala/go/massage/Main.scala
+++ src/main/scala/com/github/sguzman/scala/go/massage/Main.scala
@@ -1,7 +1,28 @@
 package com.github.sguzman.scala.go.massage
 
+import java.io.File
+import java.nio.file.{Files, Paths}
+import java.util.Base64
+
 object Main {
+
   def main(args: Array[String]): Unit = {
-    println("Hello")
+    if (args.length != 1 && args.length != 2) {
+      return
+    }
+
+    val lineLength = if (args.length == 2) args(1).toInt
+    else 10000
+
+    val fileName = args.head
+    if (!new File(fileName).exists) {
+      return
+    }
+
+    val file = Files.readAllBytes(Paths.get(fileName))
+    val encoded = Base64.getEncoder.encode(file)
+
+    val string = "const shows = [" ++ encoded.grouped(lineLength).map(a => "\"" ++ a.map(_.toChar).mkString ++  "\"").mkString(" + ") ++ "];"
+    println(string)
   }
 }
